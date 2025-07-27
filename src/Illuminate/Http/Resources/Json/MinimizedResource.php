569e4b058bdc76bb294258bbeb2b5c1edf35f100
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2025-07-27 23:45:06 +0300

    Formatting

--- src/Illuminate/Http/Resources/Json/MinimizedResource.php
+++ src/Illuminate/Http/Resources/Json/MinimizedResource.php
@@ -2,15 +2,13 @@
 
 namespace Illuminate\Http\Resources\Json;
 
-use Illuminate\Http\Resources\Json\JsonResource;
-
 abstract class MinimizedResource extends JsonResource
 {
     /**
      * MinimizedResource constructor.
      *
-     * @param mixed $resource     The resource data being transformed.
-     * @param array $onlyFields   Optional list of specific fields to include in the response.
+     * @param  mixed  $resource  The resource data being transformed.
+     * @param  array  $onlyFields  Optional list of specific fields to include in the response.
      */
     public function __construct($resource, protected array $onlyFields = [])
     {
@@ -21,8 +19,8 @@ abstract class MinimizedResource extends JsonResource
     /**
      * Determine whether the given field key should be included in the response.
      *
-     * @param string $key   The name of the field being checked.
-     * @return bool         True if no filtering is applied or the key is in the allowed list.
+     * @param  string  $key  The name of the field being checked.
+     * @return bool True if no filtering is applied or the key is in the allowed list.
      */
     protected function show(string $key): bool
     {
@@ -34,6 +32,6 @@ abstract class MinimizedResource extends JsonResource
      */
     public static function collectionWithOnly($resources, array $only = [])
     {
-        return collect($resources)->map(fn($item) => new static($item, $only));
+        return collect($resources)->map(fn ($item) => new static($item, $only));
     }
 }
